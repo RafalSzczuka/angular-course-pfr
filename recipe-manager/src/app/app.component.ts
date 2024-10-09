a9b61c0b9b226a4251c9bbdea65c22a1b6e2cb54
Piotr <Piotr.fraszczak@hotmail.com>
2024-10-09 20:08:48 +0200

    todo + clean up + first component (recipe-list)

--- recipe-manager/src/app/app.component.ts
+++ recipe-manager/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RecipeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ recipe-manager/src/app/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.scss'
+})
+export class RecipeListComponent {
+  recipes = [
+    { title: 'Spaghetti Carbonara', description: 'Klasyczne włoskie danie.' },
+    { title: 'Pancakes', description: 'Puszyste naleśniki z syropem klonowym.' },
+    { title: 'Tacos', description: 'Meksykańskie tacos z wołowiną i salsą.' }
+  ];
+}
