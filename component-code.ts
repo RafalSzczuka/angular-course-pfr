c37abc3253a7ff9886255d772f18d86df6138b6f
Piotr <Piotr.fraszczak@hotmail.com>
2024-11-01 16:18:49 +0100

    przykłady kodu

--- component-code.ts
+++ component-code.ts
@@ -1,130 +1,100 @@
-// CZĘŚĆ Template Driven Forms
+// ######################### app.routes.ts - krok 1 #########################
 
-// KROK 4 - LOGIKA KOMPONENTU RecipeTemplateFormComponent
+export const routes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' }, // domyślna ścieżka, która przekierowuje na /recipes jeśli nie ma podanej innej ścieżki.
+  { path: 'recipes', component: RecipeListComponent }, // ścieżka do widoku listy przepisów.
+  { path: 'recipe/add', component: RecipeReactiveFormComponent }, // ścieżka do formularza dodawania nowego przepisu.
+  { path: 'recipe/edit/:id', component: RecipeReactiveFormComponent }, // ścieżka do formularza edycji przepisu, gdzie :id jest dynamicznym parametrem.
+  { path: 'recipe/:id', component: RecipeDetailComponent } //  ścieżka do szczegółów wybranego przepisu.
+];
 
-// ...
-export class RecipeTemplateFormComponent implements OnInit {
-  // Normalnie moglibyśmy nie pisać tutaj typu bo po co
-  // Robimy to explicite  w celach dydaktycznych
-  showForm: boolean = false;
+// ######################### #########################
 
-  // wartość wejściowa komponentu która odpowie na pytanie czy dodajemy nowy przepis czy edytujemy już istniejący.
-  @Input() isEditMode = false;
-  // Jeżeli edytujemy, to potrzebujemy przepis, po to jest ta wartość wejściowa (opcjonalna)
-  @Input() currentRecipe: RecipeModel | null = null;
 
-  // Dzięki konstruktorowi wstzrzykniemy zależność (zasób) RecipeService
-  constructor(private recipeService: RecipeService) {}
 
-  // onInit life cycle hook przyda się gdy będziemy przygotowywać logikę edycji przepisu
+// ######################### recipe-details-component.ts #########################
+
+export class RecipeDetailComponent implements OnInit {
+  recipe: RecipeModel | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
+
   ngOnInit(): void {
-    if (this.currentRecipe) {
-      // Jeśli edytujemy, wypełnij formularz danymi przepisu
-    }
-  }
-
-  // Metoda do pokazania/ukrycia formularza
-  toggleForm(): void {
-      this.showForm = !this.showForm;
-  }
-
-  // Metoda do zatwierdzenia zmian (submit) formularza
-  onSubmit(form: NgForm): void {
-    // chcemy zmienić / dodać przepis jedynie gdy formularz nie zawiera błędów
-    if (form.valid) {
-      // tworzymy instancje przepisu (RecipeModel)
-      const newRecipe: RecipeModel = {
-        id: this.isEditMode ? this.currentRecipe!.id : Date.now(), //id musi być unikalny
-        title: form.value.title,
-        description: form.value.description,
-        ingredients: form.value.ingredients.split(','),
-        preparationTime: 30, // na razie nie mamy kontrolki, dodajemy predefiniowaną wartość
-        difficulty: 'easy' // na razie nie mamy kontrolki, dodajemy predefiniowaną wartość
-      };
-
-      // Serwis zawiera osobne metody na dodanie i edycje przepisu dlatego
-      if (this.isEditMode) {
-        this.recipeService.editRecipe(newRecipe);
-      } else {
-        this.recipeService.addRecipe(newRecipe);
-      }
-
-      // Gdy już wszystkie czynności wymagane do dodania lub edycji przepisu są wykonane, musimy zresetować formularz i ukryć komponent
-      form.reset();
-      this.showForm = false;
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.recipe = this.recipeService.getRecipeById(+id);
     }
   }
 }
 
+// ######################### #########################
 
 
 
+// ######################### recipe-reactive-form-component.ts #########################
 
-// CZĘŚĆ Reactive Forms
-
-
-// ...
-export class RecipeFormComponent implements OnInit {
-  @Input() isEditMode = false;
-  @Input() currentRecipe: RecipeModel | null = null;
-
-  showForm = false;
+@Component({
+  selector: 'app-recipe-reactive-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterLink, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule],
+  templateUrl: './recipe-reactive-form.component.html',
+  styleUrl: './recipe-reactive-form.component.scss'
+})
+export class RecipeReactiveFormComponent {
+  isEditMode = false;
+  currentRecipe: RecipeModel | undefined;
   recipeFormGroup!: FormGroup;
+  popularIngredients: string[] = [];
 
-  // wstrzykujemy FormBuildera, to dzięki niemu będziemy w stanie zbudować reactive forms
-  constructor(private fb: FormBuilder, private recipeService: RecipeService) {}
+  constructor(private fb: FormBuilder, private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) {}
 
-  // definiujemy nasza formGroup dzięki FormBuilder'owi, zauważ że na tym poziomie defiuniujesz strukturę, wartośc początkową, walidatory i tak dalej.
   ngOnInit(): void {
     this.recipeFormGroup = this.fb.group({
       title: ['', [Validators.required, Validators.minLength(3)]],
       description: ['', Validators.required],
-      ingredients: ['', Validators.required]
+      ingredients: [[], Validators.required],
+      preparationTime: ['', Validators.required],
+      difficulty: ['', Validators.required]
     });
 
-    // metoda patchValue inteligętnie podmienia wartości kontrolek podanych jako wartość wejściowa funkcji
+    this.popularIngredients = this.recipeService.getPopularIngredients();
+
+    // jeżeli istnieje przepis to go ustawiamy - tryb edycji
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      // ustawiamy edit mode
+      this.isEditMode = true;
+      this.currentRecipe = this.recipeService.getRecipeById(+id);
+      if (this.currentRecipe) {
+        this.currentRecipe.id = +id;
+      }
+    }
+
+    // jeżeli jest przepis, ustawiamy obecne wartości w formularzu
     if (this.currentRecipe) {
+      // zadziała dla niemal wszystkich typów kontrolek, nie zadziała dla formArray
       this.recipeFormGroup.patchValue(this.currentRecipe);
     }
   }
 
-  toggleForm(): void {
-    this.showForm = !this.showForm;
-  }
-
   onSubmit(): void {
     if (this.recipeFormGroup.valid) {
-      const newRecipe: RecipeModel = {
-        id: this.isEditMode ? this.currentRecipe!.id : Date.now(),
-        ...this.recipeFormGroup.value, // ta linijka tworzy 'shadow copy' obecnych wartości formularza i je tu wrzuca, dzięki temu nie musimy ich deklarować ręcznie jeżeli się nie zmianiają, nie są mapowane, parsowane itp.
-        ingredients: this.recipeFormGroup.value.ingredients.split(','), // metoda pomocnicza split(',') znajduje w ciągu znaków ',' i na tej podstawie rozdziela ciąg na części
-        preparationTime: 30,
-        difficulty: 'easy'
-      };
-
+      const recipe: RecipeModel = this.recipeFormGroup.value; // Zbieramy dane formularza
       if (this.isEditMode) {
-        this.recipeService.editRecipe(newRecipe);
+        if (this.currentRecipe) {
+          recipe.id = this.currentRecipe?.id;
+        }
+        this.recipeService.editRecipe(recipe) // Wysyłanie danych do serwisu w postaci edycji istniejącego przepisu
       } else {
-        this.recipeService.addRecipe(newRecipe);
+      this.recipeService.addRecipe(recipe); // Wysyłanie danych do serwisu w postaci nowego przepisu
       }
 
-      this.recipeFormGroup.reset();
-      this.showForm = false;
+      this.router.navigate(['/recipes']); // Powrót do listy przepisów
     }
   }
 }
 
-
-
-
-// CZĘŚĆ Angular Material
-
-  // Lista popularnych składników
-  popularIngredients: string[] = [
-    'Tomatoes', 'Onions', 'Garlic', 'Potatoes', 'Carrots', 'Olive oil', 'Butter',
-    'Chicken', 'Beef', 'Pork', 'Salt', 'Pepper', 'Paprika', 'Basil', 'Parsley',
-    'Oregano', 'Lemon', 'Sugar', 'Flour', 'Eggs', 'Milk', 'Cheese', 'Cream',
-    'Bread', 'Rice', 'Pasta', 'Beans', 'Lettuce', 'Spinach', 'Broccoli', 'Mushrooms',
-    'Fish', 'Shrimp', 'Soy sauce', 'Vinegar', 'Honey', 'Peppers', 'Zucchini', 'Cucumber',
-    'Corn', 'Chili powder'
-  ];
+// ######################### #########################
